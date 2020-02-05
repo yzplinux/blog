332cfedba5dc55b7b728f451b2dac917b386eeb2
yzp <364644384@qq.com>
2020-02-05 17:56:06 +0800

    添加了主页 修复了settings里的LANGUAGE_CODE: 将zh_hans 修复成zh-hans 否则gunicorn会报错

--- django_app/settings/common.py
+++ django_app/settings/common.py
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh_hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
